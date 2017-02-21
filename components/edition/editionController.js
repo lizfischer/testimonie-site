b2a6346412577243079c8e3bc01459190db30d60
lizfischer <lizzy.m.fischer@gmail.com>
2017-02-21 03:58:24 -0800

    fixed (hopefullygit add components/edition/editionController.js) alignment issue (wait for image to load)

--- components/edition/editionController.js
+++ components/edition/editionController.js
@@ -34,41 +34,45 @@ testimonieApp.controller('EditionController', ['$scope', '$routeParams', 'Page',
             }
         });
 
-        Page.get({pg: $routeParams.pg}, {}, function(annotations){
-            var leftTranscript = [];
-            var leftModern = [];
-            var transcript = annotations.transcript;
-            for (var i = 0; i < transcript.length; i++) {
-                var coords = getCoords(transcript[i]["on"]);
-                var text = transcript[i]["resource"]["chars"];
-                var lang = transcript[i]["resource"]["language"];
-                leftTranscript.push({text:text, coords:coords, lang:lang});
-            }
-            var modern = annotations.modern;
-            console.log(modern)
-            for (var i = 0; i < modern.length; i++) {
-                var coords = getCoords(modern[i]["on"]);
-                var text = modern[i]["resource"]["chars"];
-                var lang = modern[i]["resource"]["language"];
-                leftModern.push({text:text, coords:coords, lang:lang});
-            }
 
-            $scope.read.leftTranscript = leftTranscript;
-            $scope.read.leftModern = leftModern;
 
+        var leftImage = document.getElementById('pageLeft');
+        leftImage.addEventListener('load', function() {
+            Page.get({pg: $routeParams.pg}, {}, function(annotations){
+                var leftTranscript = [];
+                var leftModern = [];
+                var transcript = annotations.transcript;
+                for (var i = 0; i < transcript.length; i++) {
+                    var coords = getCoords(transcript[i]["on"]);
+                    var text = transcript[i]["resource"]["chars"];
+                    var lang = transcript[i]["resource"]["language"];
+                    leftTranscript.push({text:text, coords:coords, lang:lang});
+                }
+                var modern = annotations.modern;
+                console.log(modern)
+                for (var i = 0; i < modern.length; i++) {
+                    var coords = getCoords(modern[i]["on"]);
+                    var text = modern[i]["resource"]["chars"];
+                    var lang = modern[i]["resource"]["language"];
+                    leftModern.push({text:text, coords:coords, lang:lang});
+                }
+
+                $scope.read.leftTranscript = leftTranscript;
+                $scope.read.leftModern = leftModern;
+            });
         });
 
+        
         function getCoords(purl){
             var coords = {};
             var split = purl.split('#');
             split = split[1].split(',');
-            var img = document.getElementById('pageLeft');
-            //var imgWidth = img.clientWidth/1981;
-	    var imgWidth = 496/1981;
-            coords.x = parseInt(split[0])*imgWidth;
-            coords.y = parseInt(split[1])*imgWidth;
-            coords.w = parseInt(split[2])*imgWidth;
-            coords.h = parseInt(split[3])*imgWidth;
+            var widthMult = leftImage.clientWidth/1981;
+	        //var imgWidth = 496/1981;
+            coords.x = parseInt(split[0])*widthMult;
+            coords.y = parseInt(split[1])*widthMult;
+            coords.w = parseInt(split[2])*widthMult;
+            coords.h = parseInt(split[3])*widthMult;
             return coords;
         }
 
