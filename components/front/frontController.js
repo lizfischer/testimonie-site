48ab0d807edf1b9a40e19dff0b908ad1c588f401
lizfischer <lizzy.m.fischer@gmail.com>
2017-05-17 04:29:07 -0700

    adding links

--- components/front/frontController.js
+++ components/front/frontController.js
@@ -12,7 +12,7 @@ testimonieApp.controller('FrontController', ['$scope',
                 pages: [
                     {label:"The English Reformation", link:"front/reformation"},
                     {label:"Matthew Parker", link:"front/parker"},
-                    {label: "<i>A Testimonie</i> Contents", link: "front/contents"},
+                    {label:"The text", link: "front/contents"},
                     {label:"Typeface and Printing", link: "front/printing"},
                     {label:"Subsequent Publications", link:"front/reprintings"},
                     {label: "Manuscript Binding", link: "front/binding"},
